6572d24c7787fcd5952420f2e2918987406d6d01
Mats de Waard <mats.de.waard@gmail.com>
2015-08-26 15:54:51 +0200

    Base for jumping

--- Assets/MoveRight.js
+++ Assets/MoveRight.js
@@ -1,9 +1,44 @@
 ﻿#pragma strict
+
 var speed : float;
+var jumpSpeed : float;
+private var jump : boolean = true;
+private var up : boolean = false;
+private var down : boolean = false;
+
 function Start () {
 
 }
 
 function Update () {
  transform.Translate(Vector3.right * speed * Time.deltaTime);
+ 
+ do {
+ transform.Translate(Vector3.up * jumpSpeed * Time.deltaTime);
+ }
+ while(up == true);    
+ 
+ 
+ do {
+ transform.Translate(Vector3.down * jumpSpeed * Time.deltaTime);
+ }
+ while(down == true);
+ 
+ if (Input.GetKey("a") && jump==true){
+	 jumping();
+	}
+}
+
+function jumping (){
+    Debug.Log("0");
+    jump = false;
+    up = true;
+    yield WaitForSeconds(2);
+    Debug.Log("1");
+    up = false;
+    down = true;
+    yield WaitForSeconds(2);
+    Debug.Log("2");
+    down = false;
+    jump = true;
 }
