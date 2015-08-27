82cc2c7e1021475c133cb84dc0def81b50574a1d
Mats de Waard <mats.de.waard@gmail.com>
2015-08-27 10:28:17 +0200

    Player and level start

---
+++ Assets/playerColour.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var playerColour : Color32;
+
+function Start () {
+var renderer = gameObject.GetComponent(SpriteRenderer);
+renderer.color = playerColour;
+}
+
+function Update () {
+
+}
