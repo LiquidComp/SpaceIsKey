469fee57a0b3a5dbbcb84c6ee0cf2757b1e05506
Mika <mika.boersma@vathorstcollege.nl>
2015-08-27 14:10:42 +0200

    alles

--- Assets/MoveRight.js
+++ Assets/MoveRight.js
@@ -1,44 +1,46 @@
-﻿#pragma strict
+#pragma strict
 
 var speed : float;
 var jumpSpeed : float;
-private var jump : boolean = true;
-private var up : boolean = false;
-private var down : boolean = false;
+private var x = -10.0;
+public var theObject : GameObject;
+private var done = false;
+private var go = false;
 
 function Start () {
-
+theObject.transform.localPosition = Vector3.zero;
 }
 
 function Update () {
- transform.Translate(Vector3.right * speed * Time.deltaTime);
- 
- do {
- transform.Translate(Vector3.up * jumpSpeed * Time.deltaTime);
- }
- while(up == true);    
- 
- 
- do {
- transform.Translate(Vector3.down * jumpSpeed * Time.deltaTime);
- }
- while(down == true);
- 
- if (Input.GetKey("a") && jump==true){
-	 jumping();
-	}
+var nx = theObject.transform.localPosition.x; 
+transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+
+
+if(theObject.transform.localPosition.y == 0 && Input.GetKey("a")) {
+ Debug.Log("6");
+ var go = true;
+ done = true;
+
 }
 
-function jumping (){
-    Debug.Log("0");
-    jump = false;
-    up = true;
-    yield WaitForSeconds(2);
-    Debug.Log("1");
-    up = false;
-    down = true;
-    yield WaitForSeconds(2);
-    Debug.Log("2");
-    down = false;
-    jump = true;
+if(done) {
+x += 0.25f;
+Debug.Log("Done");
 }
+
+//if(go == true) {
+theObject.transform.localPosition.y = -Mathf.Pow(x, 2) + 100; 
+//}
+}
+
+function LateUpdate(){
+if(done == true && theObject.transform.localPosition.y ==0){
+go = false;
+done = false;
+x = -10.0;
+Debug.Log("go");
+
+}
+}
+
