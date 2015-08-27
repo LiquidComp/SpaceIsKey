da6f4fcfbfd35f4e71cfddd5212339cfb5b26e3c
Mika <mika.boersma@vathorstcollege.nl>
2015-08-27 09:27:55 +0200

    no message

---
+++ Assets/finishCollider.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var mainCam : Camera;
+
+var rightWall : BoxCollider2D;
+var leftWall : BoxCollider2D;
+
+function Start () {
+	rightWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height*2f, 0f)).y);
+	rightWall.offset = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x + 0.5f, 0f);
+
+	leftWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height*2f, 0f)).y);;
+	leftWall.offset = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x - 0.5f, 0f);
+
+}
+
+function Update () {
+
+}
