391e1564bc7c289532c66b3254ec5e70710115f1
Mats de Waard <dutchxedit@gmail.com>
2015-08-31 20:13:11 +0200

    Added Particle System and busy with spawning it at right location

--- Assets/obstacleRestart.js
+++ Assets/obstacleRestart.js
@@ -1,7 +1,23 @@
 ﻿#pragma strict
 
+private var particles : GameObject;
+private var ps : ParticleSystem;
+
+
+function Start() {
+	particles = GameObject.Find("deathParticle");
+	ps = GetComponentInChildren(ParticleSystem);
+}
+
 function OnCollisionEnter2D(coll: Collision2D) {
-	if (coll.gameObject.tag == "obstacle")
+	if (coll.gameObject.tag == "obstacle") {
+		 var particleVector : Vector3 = transform.localPosition;
+		 var newParticles = Instantiate(particles, particleVector, transform.rotation);
 		 transform.localPosition.x = -15.15;
 		 transform.localRotation.z = 0;
+	/*	 if(!ps.IsAlive())
+         {
+             Destroy(newParticles);
+         }
+	*/}
 }
