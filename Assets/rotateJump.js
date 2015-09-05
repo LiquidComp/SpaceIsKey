b6ec656ace4b31e743e51f9a959c5f4c476fc5e4
Mika <mika.boersma@vathorstcollege.nl>
2015-09-05 16:08:05 +0200

    dis shit

---
+++ Assets/rotateJump.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+public var player : GameObject;
+private var myInt : int;
+function Start () {
+
+}
+
+function Update () {
+
+myInt = Mathf.FloorToInt(player.transform.position.y);
+transform.Rotate(0.0f, 0.0f, myInt);
+myInt = -myInt;
+transform.Rotate(0.0f, 0.0f, myInt);
+
+}
