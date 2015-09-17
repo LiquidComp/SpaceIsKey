aeffdbbbcb501ec522e84adfbba5f9f06fd788a4
Mats de Waard <mats.de.waard@gmail.com>
2015-09-17 15:52:24 +0200

    Crack nek

--- Assets/Scripts/finishColliderMainMenu.js
+++ Assets/Scripts/finishColliderMainMenu.js
@@ -20,5 +20,19 @@ function Start () {
 	leftWall2.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height*1f, 0f)).y);
 	leftWall2.offset = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x + 0.5f, -4.4f);
 	
+	transform.position.y = block.transform.position.y;
+	transform.position.x = block.transform.position.x;
+	
+	GetComponent.<Camera>().orthographicSize = 2.0;
 }
 
+function Update () {
+if(player.GetComponent.<Rigidbody2D>().mass != 1){
+transform.position.x = Mathf.Lerp( transform.position.x, block.transform.position.x + xOffset, Time.deltaTime * smoothTime);
+transform.position.y = Mathf.Lerp( transform.position.y, block.transform.position.y + yOffset, Time.deltaTime * smoothTime);
+}
+if(player.GetComponent.<Rigidbody2D>().mass == 1){
+transform.position.x = Mathf.Lerp( transform.position.x, block.transform.position.x + -xOffset, Time.deltaTime * smoothTime);
+transform.position.y = Mathf.Lerp( transform.position.y, block.transform.position.y + yOffset, Time.deltaTime * smoothTime);
+}
+}
---
+++ Assets/Scripts/rotateJumpMainMenu.js
@@ -0,0 +1,139 @@
+﻿#pragma strict
+public var player : GameObject;
+private var myInt : float;
+private var rot1 : boolean;
+private var rot2 : boolean;
+private var startY : float;
+private var test : float;
+private var rot3 : boolean;
+private var rot4 : boolean;
+
+
+function Start () {
+	startY = 2.08;
+	rot1 = true;
+	rot2 = false;
+	rot3 = false;
+	rot4 = false;
+}
+
+function Update () {
+	test = player.GetComponent.<Rigidbody2D>().mass;
+	if(test == 1){
+		startY = -1.44;
+		rot3 = true;
+	}
+	if(test == 2){
+		startY = -4.67;
+		rot3 = false;
+		rot4 = false;
+	}
+	if((myInt <= -89) && rot1 == true && (rot3 == false && rot4 == false)) {
+		myInt = -90;
+		rot1 = false;
+		rot2 = true;
+	}
+
+	if(myInt == 270 && rot2 == true && (rot3 == false && rot4 == false)) {
+		rot2 = false;
+		myInt = 0;
+		rot1 = true;
+	}
+
+	if(rot1 == true && (rot3 == false && rot4 == false)){
+		myInt = -player.transform.position.y;
+		myInt = myInt + startY;
+		myInt = myInt * 45;
+		//Debug.Log(myInt);
+		transform.eulerAngles = Vector3(0, 0, myInt);
+	}
+
+	if(rot2 == true && (rot3 == false && rot4 == false)){
+		myInt = player.transform.position.y;
+		myInt = myInt - startY;
+		myInt = myInt * 45 + 270;
+		//Debug.Log(myInt);
+		transform.eulerAngles = Vector3(0, 0, myInt);
+	}
+
+
+	if(rot2 == true && (rot3 == false && rot4 == false)){
+		myInt = player.transform.position.y;
+		myInt = myInt - startY;
+		myInt = myInt * 45 + 270;
+		//Debug.Log(myInt);
+		transform.eulerAngles = Vector3(0, 0, myInt);
+	}
+
+	if(rot2 == true && (rot3 == false && rot4 == false)){
+		myInt = player.transform.position.y;
+		myInt = myInt - startY;
+		myInt = myInt * 45 + 270;
+		//Debug.Log(myInt);
+		transform.eulerAngles = Vector3(0, 0, myInt);
+	}
+
+	if((myInt >= 89) && (rot3 == true || rot4 == true)) {
+		myInt = 90;
+		rot3 = false;
+		rot4 = true;
+	}
+
+	if(myInt == -180 && (rot3 == true || rot4 == true)) {
+		rot3 = true;
+		myInt = 0;
+		rot4 = false;
+	}
+
+	if(rot3 == true){
+		myInt = player.transform.position.y;
+		myInt = myInt - startY;
+		myInt = myInt * 45;
+		//Debug.Log(myInt);
+		transform.eulerAngles = Vector3(0, 0, myInt);
+	}
+
+
+	if(rot3 == true){
+		myInt = player.transform.position.y;
+		myInt = myInt - startY;
+		myInt = myInt * 45;
+		//Debug.Log(myInt);
+		transform.eulerAngles = Vector3(0, 0, myInt);
+	}
+
+
+	if(rot3 == true){
+		myInt = player.transform.position.y;
+		myInt = myInt - startY;
+		myInt = myInt * 45;
+		//Debug.Log(myInt);
+		transform.eulerAngles = Vector3(0, 0, myInt);
+	}
+
+	if(rot4 == true){
+		myInt = -player.transform.position.y;
+		myInt = myInt + startY;
+		myInt = myInt * 45 - 180;
+		//Debug.Log(myInt);
+		transform.eulerAngles = Vector3(0, 0, myInt);
+	}
+
+	if(rot4 == true){
+		myInt = -player.transform.position.y;
+		myInt = myInt + startY;
+		myInt = myInt * 45 - 180;
+		//Debug.Log(myInt);
+		transform.eulerAngles = Vector3(0, 0, myInt);
+	}
+
+
+	if(rot4 == true){
+		myInt = -player.transform.position.y;
+		myInt = myInt + startY;
+		myInt = myInt * 45 - 180;
+		//Debug.Log(myInt);
+		transform.eulerAngles = Vector3(0, 0, myInt);
+	}
+
+}
