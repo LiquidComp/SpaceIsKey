e98a1fe5ad09d913fb16922da1e6baf06ef8d549
Mats de Waard <mats.de.waard@gmail.com>
2015-09-18 10:26:05 +0200

    lvl 5&6

---
+++ Assets/Scripts/ObstacleScripts/blockFallDown.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+var speed : float;
+var player : GameObject;
+var startX : float;
+
+function Start () {
+ speed = 0.0;
+}
+
+function Update () {
+	transform.Translate(Vector3.down * speed * Time.deltaTime);
+	transform.localRotation.z = 0;
+	player.transform.localRotation.z = 0;
+	transform.localPosition.x = startX;
+}
+
+function OnCollisionEnter2D(coll: Collision2D) {
+	if (coll.gameObject.tag == "obstacleGoLeft"){
+		speed = 20.0;
+	}else if(coll.gameObject.tag == "obstacleGoRight"){
+		speed = 0.0;
+	}
+}
