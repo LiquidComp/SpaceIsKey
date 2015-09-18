e9adde4a4ba661d90cc3ed57f8fbbe05c67701ad
Mika Boersma <mika8117@gmail.com>
2015-09-18 23:34:27 +0200

    Space is key text

---
+++ Assets/CenterText.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+transform.position = Camera.main.ScreenToWorldPoint( Vector3(Screen.width/2, Screen.height/2, Camera.main.nearClipPlane) );
+}
