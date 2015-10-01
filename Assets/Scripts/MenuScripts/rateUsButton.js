502c0f52ab26b888eb98992650d4b594fd5f14e1
Mats de Waard <mats.de.waard@gmail.com>
2015-10-01 11:35:20 +0200

    Main Menu almost done

---
+++ Assets/Scripts/MenuScripts/rateUsButton.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+public var playButton : Sprite;
+public var playButton2 : Sprite;
+private var mouseIsDown : boolean;
+private var notOffScreen : boolean;
+
+function Update(){
+	if(mouseIsDown == true){
+		GetComponent(SpriteRenderer).sprite = playButton2;
+	}else if(mouseIsDown == false){
+		GetComponent(SpriteRenderer).sprite = playButton;
+	}
+
+}
+
+function OnMouseOver(){
+	notOffScreen = true;
+}
+
+function OnMouseExit(){
+	notOffScreen = false;
+}
+
+
+function OnMouseUp(){
+	mouseIsDown = false;
+	if(notOffScreen == true){
+		// OPEN RATE US SCREEN SOMEHOW
+	}
+}
+
+function OnMouseDown(){
+		mouseIsDown = true;
+}
---
+++ Assets/Scripts/MenuScripts/settingsButton.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+public var rateUsButton : Sprite;
+public var rateUsButton2 : Sprite;
+private var mouseIsDown : boolean;
+private var notOffScreen : boolean;
+
+function Update(){
+	if(mouseIsDown == true){
+		GetComponent(SpriteRenderer).sprite = rateUsButton2;
+	}else if(mouseIsDown == false){
+		GetComponent(SpriteRenderer).sprite = rateUsButton;
+	}
+
+}
+
+function OnMouseOver(){
+	notOffScreen = true;
+}
+
+function OnMouseExit(){
+	notOffScreen = false;
+}
+
+
+function OnMouseUp(){
+	mouseIsDown = false;
+	if(notOffScreen == true){
+		//OPEN SETTINGS INTERFACE
+	}
+}
+
+function OnMouseDown(){
+		mouseIsDown = true;
+}
