d01b82c96853bd896c979e335fc6641d8b30438d
Mika <mika.boersma@vathorstcollege.nl>
2015-10-02 13:48:00 +0200

    If this dunt work me suicide

--- Assets/Levels/CourseSelect/Course1/C1L7.js
+++ Assets/Levels/CourseSelect/Course1/C1L7.js
@@ -1,16 +1,4 @@
 ﻿#pragma strict
-
-private var mouseDown : boolean = false;
-
-function OnMouseDown(){
-     if(mouseDown == false){
-   		Application.LoadLevel("C1L7");
-     	mouseDown = true;
- 	 }
-}
-
 function OnMouseUp(){
-	if(mouseDown == true){
-		mouseDown = false;
-	}
+	Application.LoadLevel("C1L7");
 }
