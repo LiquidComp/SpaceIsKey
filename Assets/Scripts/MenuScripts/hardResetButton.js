e1baf7d18e793cc76868e30a7c2779e6bfd4a30b
Mats de Waard <mats.de.waard@gmail.com>
2015-11-01 16:18:21 +0100

    Hard Reset Confirm Box

---
+++ Assets/Scripts/MenuScripts/hardResetButton.js
@@ -0,0 +1,41 @@
+﻿#pragma strict
+
+public var resetButton : Sprite;
+public var resetButton2 : Sprite;
+private var mouseIsDown : boolean;
+private var notOffScreen : boolean;
+public var hardResetConfirmScript : hardResetConfirm;
+public var object1 : GameObject;
+public var object2 : GameObject;
+
+function Update(){
+	if(mouseIsDown == true){
+		GetComponent(SpriteRenderer).sprite = resetButton2;
+	}else if(mouseIsDown == false){
+		GetComponent(SpriteRenderer).sprite = resetButton;
+	}
+
+}
+
+function OnMouseOver(){
+	notOffScreen = true;
+}
+
+function OnMouseExit(){
+	notOffScreen = false;
+}
+
+
+function OnMouseUp(){
+	mouseIsDown = false;
+	if(notOffScreen == true){
+	    object1.gameObject.SetActive(false);
+	    object2.gameObject.SetActive(false);
+		Time.timeScale = 0;
+		hardResetConfirmScript.hardResetIsPressed = true;
+	}
+}
+
+function OnMouseDown(){
+		mouseIsDown = true;
+}
---
+++ Assets/Scripts/MenuScripts/hardResetConfirm.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+public var skin : GUISkin;
+public var object1 : GameObject;
+public var object2 : GameObject;
+public var hardResetIsPressed : boolean;
+
+
+function OnGUI(){
+if(hardResetIsPressed == true){
+   GUI.skin = skin;
+   //GUI.Box(new Rect(0,0,Screen.width,Screen.height),"Clear progress");
+   GUI.Label(new Rect(Screen.width*1/4,Screen.height*2/6,Screen.width*2/4,Screen.height*1/6), "Do you want to reset ALL progress?");
+if(GUI.Button(Rect(Screen.width/4,Screen.height*3/8,Screen.width/2,Screen.height/8),"Yes"))
+          {
+          		PlayerPrefs.DeleteAll();
+          		Application.Quit();
+          }
+          if(GUI.Button(Rect(Screen.width/4,Screen.height*4/8,Screen.width/2,Screen.height/8),"No"))
+          {
+          object1.gameObject.SetActive(true);
+	      object2.gameObject.SetActive(true);
+          hardResetIsPressed = false;
+          Time.timeScale = 1;
+}}}
